59726058cd8567310fcd6a89cb68369f3cf163da
Ryan Keleti <github@ryankeleti.com>
2022-01-07 14:31:41 -0500

    add road-usroads weights and add to main

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,13 @@ fn ca_netscience() {
     run_script::run_script(path, 2);
 }
 
+fn road_usroads() {
+    println!("running road-usroads example...");
+    let path = Path::new("res/road-usroads/road-usroads.mtx");
+    run_script::run_script(path, 2);
+}
+
 fn main() {
     ca_netscience();
+    road_usroads();
 }
