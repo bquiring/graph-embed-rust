418cd791de115940493cce21cee6e10daf0f1198
Ryan Keleti <github@ryankeleti.com>
2022-01-13 23:00:20 -0500

    switch back to zero index

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ mod run_script;
 fn ca_netscience() {
     println!("running ca-netscience example...");
     let path = Path::new("res/ca-netscience/ca-netscience.mtx");
-    let zero_indexed = true;
-    run_script::run_script(path, zero_indexed, 2, false);
+    let zero_indexed = false;
+    run_script::run_script(path, zero_indexed, 2, true);
 }
 
 fn road_usroads() {
